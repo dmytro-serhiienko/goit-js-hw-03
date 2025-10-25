71c28f0213983d7a085f34c3713bd4b3a7098c99
Dmytro Serhiienko <conneciones@gmail.com>
2025-10-25 19:54:57 +0300

    1 task

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+function slugify(title) {
+  let lowerTitle = title.toLowerCase();
+  let splitTitle = lowerTitle.split(' ');
+  let joinTitle = splitTitle.join('-');
+
+  return joinTitle;
+}
+
+console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
